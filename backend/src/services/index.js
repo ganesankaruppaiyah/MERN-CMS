81d0985bad9ae7725505a38a3927f9b593ae4ef4
Scott Long <slong@scisoc.org>
2017-10-05 23:09:37 -0500

    Blog Backend Service

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,5 +1,7 @@
 const contact = require('./contact/contact.service.js');
+const blog = require('./blog/blog.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(contact);
+  app.configure(blog);
 };
